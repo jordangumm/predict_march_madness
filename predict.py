3e1bb076a41724f3ccaeb5c43090a817d1164ec8
Jordan Gumm <jordan.gumm@progenity.com>
2019-05-13 10:32:00 -0400

    normalizing inputs to maxout

--- predict.py
+++ predict.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from automaxout.models.maxout import Maxout
 from sklearn.linear_model     import LogisticRegression
+from sklearn.preprocessing    import StandardScaler
 from sklearn.metrics          import log_loss
 
 from datautil       import get_train_examples, get_test_examples
@@ -39,11 +40,13 @@ def maxout():
         train_y, train_X = get_train_examples(season)
         test_y, test_X   = get_test_examples(season)
 
-        train_X = train_X.values
-        train_y = train_y.values
+        scaler = StandardScaler()
+        scaler.fit(train_X)
 
-        test_X = test_X.values
-        test_y = test_y.values
+        train_X = scaler.transform(train_X)
+        test_X  = scaler.transform(test_X)
+        train_y = train_y.values
+        test_y  = test_y.values
 
         clf = Maxout(len(test_X[0]), verbose=True)
 
