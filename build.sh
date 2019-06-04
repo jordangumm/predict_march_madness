5a4e720fb181587c185a3a019bd2fcacd6eb8a99
Jordan Gumm <jngumm@gmail.com>
2019-06-04 01:40:40 +0000

    transition to pipenv

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+#wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+wget https://repo.continuum.io/miniconda/Miniconda3-4.6.14-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p miniconda
 rm miniconda.sh
 
 source miniconda/bin/activate
 
-pip install -r https://raw.githubusercontent.com/Lasagne/Lasagne/v0.1/requirements.txt
-pip install -r requirements.txt
+pip install pipenv
+pipenv install
+
+LD_LIBRARY_PATH=$PWD/miniconda/lib pipenv --three
+cp $PWD/miniconda/lib/libpython3.7m.so.1.0 $(pipenv --venv)/lib
