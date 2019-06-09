44844b16d81dcfe66b2f497c0b036a9b9dc39a7d
Jordan Gumm <jngumm@gmail.com>
2019-06-09 21:42:19 +0000

    updated build for cuda support

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
-#wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 wget https://repo.continuum.io/miniconda/Miniconda3-4.6.14-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p miniconda
 rm miniconda.sh
 
 source miniconda/bin/activate
 
-pip install pipenv
-pipenv install
+pip install click
+pip install deap
+pip install pandas
+pip install sklearn
+pip install tqdm
 
-LD_LIBRARY_PATH=$PWD/miniconda/lib pipenv --three
-cp $PWD/miniconda/lib/libpython3.7m.so.1.0 $(pipenv --venv)/lib
+pip install --upgrade https://github.com/Theano/Theano/archive/master.zip
+pip install --upgrade https://github.com/Lasagne/Lasagne/archive/master.zip
+pip install mkl-service
+pip install -e ../automaxout/.
+pip install -e ../cbbstats/.
